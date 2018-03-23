bcfd5cd3cb7c935a38126b0f5e2a590d56839708
na2731 <na2731@tc.columbia.edu>
2018-03-22 23:50:53 -0400

    InclassAssignment08

---
+++ GeoquizCard/app/src/main/java/com/example/android/geoquizcard/QuestionViewHolder.java
@@ -0,0 +1,53 @@
+package com.example.android.geoquizcard;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by anning on 2018/3/22.
+ */
+
+public class QuestionViewHolder extends RecyclerView.ViewHolder {
+    public CardView cardView;
+    public TextView questionCountry;
+    public TextView countryQuestion;
+    public ImageView countryImage;
+    //public TextView isTrue;
+
+
+//    Intent e=new Intent(this,MainActivity.class);
+//        e.putExtra
+
+
+    public QuestionViewHolder(View itemView, final Context context, final List<Question> questions) {
+        super(itemView);
+        cardView = (CardView) itemView.findViewById(R.id.card_view);
+        questionCountry = (TextView) itemView.findViewById(R.id.question_country);
+        countryQuestion = (TextView) itemView.findViewById(R.id.country_question);
+        countryImage = (ImageView) itemView.findViewById(R.id.imageView);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (questionCountry.getText().equals("asia")) {
+                    Toast.makeText(context, String.valueOf(questions.get(0).isTrue), Toast.LENGTH_SHORT).show();
+                }
+                if(questionCountry.getText().equals("australia")){
+                    Toast.makeText(context, String.valueOf(questions.get(2).isTrue), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
+
+
+
